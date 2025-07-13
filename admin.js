081d74c8c01811385cfc82563749a25a70b3116e
chyber-nana <nanaion.xp@gmail.com>
2025-07-13 19:08:01 +0100

    gn123433

--- admin.js
+++ admin.js
@@ -42,32 +42,32 @@ document.getElementById("loginForm").addEventListener("submit", async function (
 });
 
 // Register Handler
-document.getElementById("registerForm").addEventListener("submit", async function (e) {
-  e.preventDefault();
+// document.getElementById("registerForm").addEventListener("submit", async function (e) {
+//   e.preventDefault();
 
-  const username = document.getElementById("registerUsername").value.trim();
-  const password = document.getElementById("registerPassword").value;
-  const message = document.getElementById("messageRegister");
-  message.textContent = "Registering...";
+//   const username = document.getElementById("registerUsername").value.trim();
+//   const password = document.getElementById("registerPassword").value;
+//   const message = document.getElementById("messageRegister");
+//   message.textContent = "Registering...";
 
-  try {
-    const res = await fetch("https://novahub-backend.onrender.com/api/admin/register", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username, password })
-    });
+//   try {
+//     const res = await fetch("https://novahub-backend.onrender.com/api/admin/register", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify({ username, password })
+//     });
 
-    const data = await res.json();
+//     const data = await res.json();
 
-    if (res.ok) {
-      message.style.color = "green";
-      message.textContent = "Registration successful ✅";
-    } else {
-      message.style.color = "red";
-      message.textContent = data.message || "Registration failed";
-    }
-  } catch (err) {
-    message.textContent = "Server error";
-    console.error(err);
-  }
-});
+//     if (res.ok) {
+//       message.style.color = "green";
+//       message.textContent = "Registration successful ✅";
+//     } else {
+//       message.style.color = "red";
+//       message.textContent = data.message || "Registration failed";
+//     }
+//   } catch (err) {
+//     message.textContent = "Server error";
+//     console.error(err);
+//   }
+// });
